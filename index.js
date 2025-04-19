1f34a9576aad39878383874f0780cece92d2f262
Julian Frank <julianknarf@outlook.com>
2025-04-19 14:48:32 +0000

    🎉 feat: update version to 25.4.198 in package.json and refactor exports in index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import beercssModeSwitch from "./packages/beercss-mode-switch";
-
 export {beercssModeSwitch}
 
 import js_audio_tools_bundle from "./packages/js-audio-tools/bundle/js_audio_tools_bundle"
-
 export {js_audio_tools_bundle}
+
+export default {
+    beercssModeSwitch,
+    js_audio_tools_bundle
+}
