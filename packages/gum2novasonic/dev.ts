c6df89c6b51226b1093e3a145817db1902d3619b
Julian Frank <julianknarf@outlook.com>
2025-04-28 14:54:13 +0000

    ✨ feat: update Gum2NovaSonic class with stream management methods and logging; refactor dev script and add icon to HTML

--- packages/gum2novasonic/dev.ts
+++ packages/gum2novasonic/dev.ts
@@ -1,9 +1,12 @@
 // this is just a sample code for dev purpose 
-import MicManager from "../mic_manager/bundle/mic_manager_bundle"
+import MicManager from "../mic_manager/src/MicManager"
+
+import {Gum2NovaSonic} from "./src/Gum2NovaSonic"
 console.log("Dev Script Running")
 
 // amazonq-ignore-next-line
 const mm=new MicManager({})
 mm.createMicUI({})
-// amazonq-ignore-next-line
-// console.log({mm})
+
+const gum2NovaSonic=new Gum2NovaSonic()
+mm.setStreamTarget(gum2NovaSonic.setStreamTarget())
